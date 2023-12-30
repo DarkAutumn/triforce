84f242385b884a31c62be047026acdada354a823
DarkAutumn <DarkAutumn@users.noreply.github.com>
2023-12-30 09:07:35 -0800

    Build initial DQN implementation, zelda offsets
    Begin initial zelda and DQN implementation.

---
+++ DeepQNetwork/DeepQNetwork.py
@@ -0,0 +1,167 @@
+import random
+import numpy as np
+import tensorflow as tf
+from collections import deque
+import os
+
+# model parameters
+image_height = 240
+image_width = 256
+image_channels = 3
+num_output = 8                # number of actions the agent can take
+
+# Deep Q-learning Agent parameters
+gamma = 0.95  # discount factor
+epsilon = 1.0  # exploration rate
+epsilon_min = 0.10
+epsilon_decay = 0.998
+learning_rate = 0.001
+batch_size = 32
+
+max_memory_len = 20_000            # one per frame we made a decision on
+max_decisions_per_game = 1_000
+
+num_episodes = 1001
+output_dir = 'd:/model_output/zelda'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+class DqnAgent:
+    """Deep Q-learning Agent based on Jon Krohn's video series"""
+    def __init__(self, frame_count, feature_count):
+        self.memory = deque(maxlen=2000)  # double-ended queue; acts like list, but elements can be added/removed from either end
+        
+        self.gamma = gamma  # discount rate
+        self.epsilon = epsilon  # exploration rate
+        
+        self.epsilon_min = epsilon_min  # minimum exploration probability
+        self.epsilon_decay = epsilon_decay  # exploration probability decay factor
+        
+        self.learning_rate = learning_rate
+        
+        self.model = self._build_model(frame_count, feature_count)
+
+    def _build_model(self, frame_count, feature_count):
+        # Image processing pathway with LSTM
+        image_input = tf.keras.Input(shape=(frame_count, image_height, image_width, image_channels))
+        conv_layers = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(32, (8, 8), strides=(4, 4), activation='relu'))(image_input)
+        pool_layers = tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))(conv_layers)
+        conv2_layers = tf.keras.layers.TimeDistributed(tf.keras.layers.Conv2D(64, (4, 4), strides=(2, 2), activation='relu'))(pool_layers)
+        flatten = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(conv2_layers)
+        lstm = tf.keras.layers.LSTM(64)(flatten)
+
+        # Game state pathway
+        state_input = tf.keras.Input(shape=(feature_count,))
+        dense_state = tf.keras.layers.Dense(64, activation='relu')(state_input)
+
+        # Combining pathway
+        combined = tf.keras.layers.concatenate([lstm, dense_state])
+        combined_dense = tf.keras.layers.Dense(512, activation='relu')(combined)
+
+        # Output layer
+        output = tf.keras.layers.Dense(num_output, activation='sigmoid')(combined_dense)
+
+        # Create the model
+        model = tf.keras.Model(inputs=[image_input, state_input], outputs=output)
+        model.compile(optimizer='adam', loss='binary_crossentropy')
+        
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        # state: current state
+        # action: action taken
+        # reward: reward from action
+        # next_state: next state
+        # done: whether game is done or not
+        self.memory.append((state, action, reward, next_state, done))  # append tuple to memory
+
+    def act(self, state):
+        # state: current state
+        if np.random.rand() <= self.epsilon:  # if random number from 0 to 1 is less than exploration rate
+            return np.random.beta(0.5, 0.5, num_output)
+        
+        act_values = self.model.predict(state)  # predict reward value based on current state
+        return np.argmax(act_values[0])         # return action with highest reward
+
+    def replay(self, batch_size):
+        # batch_size: size of random sample from memory
+        minibatch = random.sample(self.memory, batch_size)  # random sample from memory
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target_f = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+                target_f[0][action] = target
+                self.model.fit(state, target_f, epochs=1, verbose=0)
+                
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+            
+        if self.epsilon < self.epsilon_min:
+            self.epsilon = self.epsilon_min
+    
+    def load(self, name):
+        self.model.load_weights(name)
+        
+    def save(self, name):
+        self.model.save_weights(name)
+
+class DqnAgentRunner:
+    def __init__(self, iterations, score_function, frames, parameters):
+        # per run variables
+        self.curr_iteration = 0
+        self.total_iterations = iterations
+        self.score_function = score_function
+        
+        self.agent = DqnAgent(frames, parameters)
+        if os.path.exists(output_dir + '/weights.h5'):
+            self.agent.load(output_dir + '/weights.h5')
+        
+        # per iteration variables
+        self.prev_action = None
+        self.prev_state = None
+        self.prev_state_for_score = None
+        self.score = 0.0
+    
+    def start_iteration(self):
+        if self.curr_iteration < self.total_iterations:
+            self.curr_iteration += 1
+            return True
+        
+        self.prev_action = None
+        self.prev_state = None
+        self.prev_state_for_score = None
+        self.score = 0.0
+        
+        return False
+    
+    def end_iteration(self):
+        if self.prev_action is not None:
+            print(f"episode: {self.curr_iteration} score: {self.score}")
+            self.prev_action = None
+            
+            if len(self.agent.memory) > batch_size:
+                self.agent.replay(batch_size)
+                
+            if self.iterations_remaining % 100 == 0:
+                self.agent.save(output_dir + f"/weights_{self.curr_iteration:04d}.h5")
+
+    def next_action(self, state_for_model, state_for_score, done):
+        if self.prev_action is not None:
+            self.agent.remember(self.prev_state, self.prev_action, self.prev_reward, state_for_model, done)
+            
+        if done:
+            self.end_iteration()
+            return None
+        
+        # calculate next action
+        action = self.agent.act(state_for_model)
+        
+        # update variables for next iteration
+        self.prev_action = action
+        self.prev_reward = self.score_function(self.prev_state_for_score, state_for_score)
+        self.score += self.prev_reward
+        
+        self.prev_state = state_for_model
+        self.prev_state_for_score = state_for_score
+        
+        return action
---
+++ DeepQNetwork/zelda.py
@@ -0,0 +1,168 @@
+import ctypes
+import numpy as np
+from collections import deque
+from DeepQNetwork import DqnAgentRunner
+
+# number of frames to give the model
+model_parameter_count = 0
+model_frame_count = 5
+frames_saved = 60
+
+reward_small = 1.0
+reward_medium = 10.0
+reward_large = 50.0
+
+penalty_small = -1.0
+penalty_medium = -10.0
+penalty_large = -50.0
+
+class ZeldaMemoryLayout:
+    def __init__(self):
+        self.locations = [
+            ( 0x10, "dungeon"),
+            ( 0xeb, "location"),
+            ( 0x12, "mode"),
+            (0x671, "triforce"),
+            (0x66f, "hearts_and_containers"),
+            (0x670, "partial_hearts"),
+            (0x66d, "rupees"),
+            (0x67d, "rupees_to_add"),
+            (0x658, "bombs"),
+            (0x67C, "bombMax"),
+            (0x657, "sword"),
+            (0x65A, "bow"),
+            (0x65B, "candle"),
+            (0x65C, "whistle"),
+            (0x65D, "food"),
+            (0x65E, "potion"),
+            (0x65F, "magic_rod"),
+            (0x660, "raft"),
+            (0x661, "magic_book"),
+            (0x662, "ring"),
+            (0x663, "step_ladder"),
+            (0x664, "magic_Key"),
+            (0x665, "power_braclet"),
+            (0x666, "letter"),
+            (0x667, "compass"),
+            (0x668, "map"),
+            (0x669, "compass9"),
+            (0x66A, "map9"),
+            (0x66C, "clock"),
+            (0x66C, "keys"),
+            (0x674, "regular_boomerang"),
+            (0x675, "magic_boomerang"),
+            ]
+        
+        self.index_map = {name: index for index, (_, name) in enumerate(self.locations)}
+
+    def get_index(self, name):
+        if name in self.index_map:
+            return self.index_map[name]
+        
+memoryLayout = ZeldaMemoryLayout()
+
+
+class ZeldaMemory:
+    def __init__(self, snapshot):
+        self.snapshot = snapshot
+        
+    def __getattr__(self, item):
+        if item in self._dict:
+            return self._dict[item]
+        
+        return self.snapshot[memoryLayout.get_index(item)]
+    
+    @property
+    def triforce_pieces(self):
+        return np.binary_repr(self.triforce).count('1')
+    
+    @property
+    def heart_containers(self):
+        return (self.hearts_and_containers >> 4) + 1
+    
+    @property
+    def hearts(self):
+        full_hearts = (self.hearts_and_containers & 0x0F) + 1
+        partialHealth = self.partial_hearts
+        if partialHealth > 0xf0:
+            return full_hearts
+        
+        return full_hearts - 1 + partialHealth / 16.0
+    
+    @property
+    def boomerang(self):
+        if self.regular_boomerang != 0:
+            return 0.5
+        elif self.magic_boomerang != 0:
+            return 1.0
+        else:
+            return 0.0
+            
+
+class MemoryWrapper:
+    def __init__(self, addr):
+        """Takes in a raw address in memory to a byte array of the Zelda memory locations.  This should
+        be in the exact order that ZeldaMemoryConstants.locations are in"""
+        self.pointer = ctypes.cast(addr, ctypes.POINTER(ctypes.c_uint8 * len(memoryLayout.locations)))
+
+    def snapshot(self) -> ZeldaMemory:
+        return ZeldaMemory(np.frombuffer(self.pointer.contents, dtype=np.uint8))
+
+
+class ScreenWrapper:
+    def __init__(self, addr, width = 256, height = 240):
+        """A wrapper around ARGB screen pixels"""
+        self.depth = 4 # ARGB
+        self.pointer = ctypes.cast(addr, ctypes.POINTER(ctypes.c_uint8 * width * height * self.depth))
+        self.width = width
+        self.height = height
+        
+        
+    def snapshot(self) -> np.ndarray:
+        return np.frombuffer(self.pointer.contents, dtype=np.uint8).reshape((self.height, self.width, 4))
+
+no_action = [False] * 8
+class LegendOfZeldaAgent:
+    def __init__(self, memoryAddress, screenAddress, num_iterations):
+        self.memory = MemoryWrapper(memoryAddress)
+        self.screen = ScreenWrapper(screenAddress)
+        
+        # keep the last 60 seconds of frames
+        self.frames = deque(maxlen=60)
+        
+        self.frame_sequence = []
+        curr = frames_saved * 2
+        for i in range(0, model_frame_count - 1):
+            self.frame_sequence.append(-curr / 2)
+        self.frame_sequence.append(-1)
+        
+        self.dqn_agent = DqnAgentRunner(num_iterations, self.score, model_frame_count, model_parameter_count)
+        if not self.dqn_agent.start_iteration():
+            raise Exception("Unable to start first iteration")
+
+    def on_frame(self) -> [bool]:
+        frame = self.screen.snapshot()
+        self.frames.append(frame)
+        if len(self.frames) < frames_saved:
+            return no_action
+        
+        frames = np.stack(self.enumerate_frames(model_frame_count), axis=0)
+        
+        # todo: build model_state, normalize model_state, set model_parameter_count
+        memory = self.memory.snapshot()
+        
+
+        action_probabilities = self.dqn_agent.next_action([frames, model_state], memory)
+        return [x > 0.6 for x in action_probabilities]
+        
+    def enumerate_frames(self, num_frames):
+        # when we just start the session, we won't have enough frames yet
+
+        if len(self.frames) < frames_saved:
+            return [self.frames[0]] * num_frames
+        
+        return (self.frames[i] for i in self.frame_sequence)
+        
+
+    def score(self, old_state : ZeldaMemory, new_state : ZeldaMemory) -> float:
+        pass
