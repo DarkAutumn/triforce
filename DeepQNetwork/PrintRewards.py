3b009a4d6c87a5929cdf1722fbaceeda59ebc5e7
Dark Autumn <DarkAutumn@users.noreply.github.com>
2023-12-30 21:17:54 -0800

    Split scoring into its own class

---
+++ DeepQNetwork/PrintRewards.py
@@ -0,0 +1,5 @@
+# a test script to print rewards to test scoring
+from zelda import MemoryWrapper, LegendOfZeldaScorer
+import emu
+
+# todo
--- DeepQNetwork/zelda.py
+++ DeepQNetwork/zelda.py
@@ -18,7 +18,14 @@ penalty_small = -1.0
 penalty_medium = -10.0
 penalty_large = -50.0
 
+class Logger:
+    def log(self, message):
+        print(message)
+
+logger = Logger()
+
 class ZeldaGameStates:
+    """An enum of game states"""
     def __init(self):
         self.titleTransition = 0
         self.selectionScreen = 1
@@ -34,20 +41,30 @@ class ZeldaGameStates:
 zeldaGameStates = ZeldaGameStates()        
 
 class ZeldaMemoryLayout:
+    """Raw memory addresses in the game and what they map to"""
     def __init__(self):
         self.locations = [
             ( 0x10, "dungeon"),
-            ( 0x12, "game_state"),
             ( 0xeb, "location"),
-            ( 0x12, "mode"),
+            ( 0x12, "game_state"),
+
+            ( 0xb9, "sword_animation"),
+            ( 0xba, "beam_animation"),
+            ( 0xbc, "bomb_or_flame_animation"),
+            ( 0xbd, "bomb_or_flame_animation2"),
+            ( 0xbe, "arrow_magic_animation"),
+            
             (0x671, "triforce"),
             (0x66f, "hearts_and_containers"),
             (0x670, "partial_hearts"),
+            
             (0x66d, "rupees"),
             (0x67d, "rupees_to_add"),
+            
             (0x658, "bombs"),
             (0x67C, "bombMax"),
             (0x657, "sword"),
+            (0x659, "arrows"),
             (0x65A, "bow"),
             (0x65B, "candle"),
             (0x65C, "whistle"),
@@ -61,15 +78,21 @@ class ZeldaMemoryLayout:
             (0x664, "magic_Key"),
             (0x665, "power_braclet"),
             (0x666, "letter"),
+            (0x674, "regular_boomerang"),
+            (0x675, "magic_boomerang"),
+            
             (0x667, "compass"),
             (0x668, "map"),
             (0x669, "compass9"),
             (0x66A, "map9"),
+            
             (0x66C, "clock"),
+            
             (0x66C, "keys"),
-            (0x674, "regular_boomerang"),
-            (0x675, "magic_boomerang"),
+            
             (0x656, "selected_item"),
+            
+            (0x627, "room_kill_count"),
             ]
         
         self.index_map = {name: index for index, (_, name) in enumerate(self.locations)}
@@ -146,6 +169,72 @@ class ScreenWrapper:
     def snapshot(self) -> np.ndarray:
         return np.frombuffer(self.pointer.contents, dtype=np.uint8).reshape((self.height, self.width, 4))
 
+
+class LegendOfZeldaScorer:
+    def __init__(self):
+        self._locations = set()
+        
+        self.reward_new_location = reward_medium
+        self.reward_enemy_kill = reward_small
+        self.reward_get_rupee = reward_small
+        self.reward_gain_health = reward_medium     # always prioritize health
+
+        self.penalty_lose_beams = penalty_medium
+        self.penalty_take_damage = penalty_small
+        self.penalty_game_over = penalty_large
+        
+    def __hearts_equal(self, first, second):
+        return abs(first - second) <= 0.01
+    
+    def score(self, old_state : ZeldaMemory, new_state : ZeldaMemory) -> float:
+        reward = 0
+
+        # has the agent found a brand new place?
+        old_location = (old_state.level, old_state.location)
+        new_location = (new_state.level, new_state.location)
+        
+        if old_location != new_location and new_location not in self._locations:
+            if self._locations.add(new_location):
+                reward += self.reward_new_location
+                logger.log(f"Reward for discovering new room! {self.reward_new_location}")
+                
+        # did link kill an enemy?
+        if old_state.room_kill_count < new_state.room_kill_count:
+            reward += self.reward_enemy_kill
+            logger.log(f"Reward for killing an enemy!")
+            
+        # did link gain rupees?
+        if old_state.rupees_to_add < new_state.rupees_to_add:
+            # rupees are added to the total one at a time when you pick up multiple, so we
+            # only reward for the accumulator that adds them, not the value of the current
+            # rupee total
+            reward += self.reward_get_rupee
+            logger.log(f"Reward for gaining rupees!")
+
+        # did link gain health?
+        if old_state.hearts < new_state.hearts:
+            reward += self.reward_gain_health
+            logger.log(f"Reward for gaining hearts!")
+            
+        # did link lose health?
+        elif old_state.hearts > new_state.hearts:
+            # did link lose sword beams as a result?
+            if self.__hearts_equal(old_state.hearts, old_state.heart_containers):
+                reward += self.penalty_lose_beams
+                logger.log("Penalty for losing beams!");
+            
+            # losing anything other than the first or last heart is less of an issue
+            else:
+                reward += self.penalty_take_damage
+                logger.log("Penalty for losing health!")
+        
+        # did we hit a game over?
+        if old_state.game_state != zeldaGameStates.game_over and new_state.game_state == zeldaGameStates.game_over:
+            reward += self.penalty_game_over
+            logger.log("Penalty for game over!")
+            
+        return reward
+
 no_action = [False] * 8
 class LegendOfZeldaAgent:
     def __init__(self, memoryAddress, screenAddress, num_iterations):
@@ -157,7 +246,7 @@ class LegendOfZeldaAgent:
         self.frames = deque(maxlen=60)
         self.shadow_frames = deque(maxlen = model_frame_count)
         
-        self.dqn_agent = DqnAgentRunner(num_iterations, self.score, model_frame_count, model_parameter_count)
+        self.dqn_agent = DqnAgentRunner(num_iterations, LegendOfZeldaScorer().score, model_frame_count, model_parameter_count)
         if not self.dqn_agent.start_iteration():
             raise Exception("Unable to start first iteration")
 
@@ -177,7 +266,7 @@ class LegendOfZeldaAgent:
             if self.last_game_state is None or self.last_game_state.game_state != zeldaGameStates.gameplay:
                 self.frames.clear()
                 
-            self.frames.append(frame)
+            self.frames.append(self.current_frame)
         
         else:
             # use the shadow frame sequence for non-gameplay frames
@@ -185,14 +274,20 @@ class LegendOfZeldaAgent:
             if self.last_game_state is None or self.last_game_state.game_state != state:
                 self.shadow_frames.clear()
             
-            self.shadow_frames.append(frame)
+            self.shadow_frames.append(self.current_frame)
         
+        # report back that link is animation locked and should not try to get an action
+        sword_animation = self.current_game_state.sword_animation
+        if sword_animation == 1 or sword_animation == 2:
+            return zeldaGameStates.gameplay_animation_lock
         
-        # todo: handle and return gameplay_animation_lock
+        # same for wand
+        if sword_animation == 31 or sword_animation == 32:
+            return zeldaGameStates.gameplay_animation_lock
         
         return state
 
-    def get_action_from_game_state(self) -> [bool]:
+    def get_action_from_game_state(self):
         """Returns either a controller state, or None if the game is over."""
         if len(self.frames) < frames_saved:
             return no_action
@@ -211,8 +306,7 @@ class LegendOfZeldaAgent:
             
             ]
         
-
-        action_probabilities = self.dqn_agent.next_action([frames, model_state], memory)
+        action_probabilities = self.dqn_agent.next_action([frames, model_state], self.current_game_state.game_state == zeldaGameStates.game_over)
         if action_probabilities is not None:
             action_probabilities = [x > 0.6 for x in action_probabilities]
         
@@ -231,9 +325,4 @@ class LegendOfZeldaAgent:
             return [self.frames[0]] * num_frames
         
         return (self.frames[i] for i in self.frame_sequence)
-        
-
-    def score(self, old_state : ZeldaMemory, new_state : ZeldaMemory) -> float:
-        #todo: define score
-        # if entered dungeon and that triforce not obtained, add reward
         
