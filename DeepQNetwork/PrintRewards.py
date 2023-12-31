75933380d54bb826a797cb5502940301e3287780
DarkAutumn <DarkAutumn@users.noreply.github.com>
2023-12-31 09:01:01 -0800

    Cleanup rewards test

--- DeepQNetwork/PrintRewards.py
+++ DeepQNetwork/PrintRewards.py
@@ -2,46 +2,31 @@
 import emu
 import zelda
 import importlib
+
+# Reload so that if I make live changes to zelda.py they are reflected in Mesen
 importlib.reload(zelda)
 
-last_hearts = 0.0
-def onFrame(cpuType):
-    try:
-        global last_snapshot
-        global last_hearts
-        curr_snapshot = zelda_memory.snapshot()
-        
-        if curr_snapshot.hearts != last_hearts:
-            print(curr_snapshot.hearts)
-            last_hearts = curr_snapshot.hearts
-
-        if last_snapshot.hearts != curr_snapshot.hearts:
-            print("found!")
-
-        if last_snapshot is not None:
-            reward = rewards.score(last_snapshot, curr_snapshot)
+class PrintRewards:
+    def __init__(self):
+        addr = emu.registerFrameMemory(7, zelda.memoryLayout.get_memory_list())
+        self.zelda_memory = zelda.MemoryWrapper(addr)
+        self.last_snapshot = self.zelda_memory.snapshot()
+        self.rewards = zelda.LegendOfZeldaScorer()
+         
+    def onFrame(self, cpuType):
+        try:
+            curr_snapshot = self.zelda_memory.snapshot()
+            reward = self.rewards.score(self.last_snapshot, curr_snapshot)
             if reward > 0.1:
                 print(f"reward: {reward}")
-         
-        last_snapshot = curr_snapshot
-    except Exception as e:
-        print(e)
-        import traceback
-        print(traceback.format_exc())
         
-        emu.removeEventCallback(onFrame, eventType.startFrame)
+            self.last_snapshot = curr_snapshot
+        except Exception as e:
+            print(e)
+            import traceback
+            print(traceback.format_exc())
+            
+            emu.removeEventCallback(self.onFrame, self.eventType.startFrame)
 
-addr = emu.registerFrameMemory(7, zelda.memoryLayout.get_memory_list())
-zelda_memory = zelda.MemoryWrapper(addr)
-last_snapshot = zelda_memory.snapshot()
-rewards = zelda.LegendOfZeldaScorer()
-print("registered frame mem")
 
-emu.addEventCallback(onFrame, eventType.startFrame)
-
-class TestClass:
-	def onFrame(self, count):
-		print("here!")
-		
-emu.addEventCallback(TestClass().onFrame, eventType.startFrame)
-		
+emu.addEventCallback(PrintRewards().onFrame, eventType.startFrame)
