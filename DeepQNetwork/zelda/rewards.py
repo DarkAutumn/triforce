52f40fbee1b511e18bb2568f62c2787fc5fffaa7
DarkAutumn <DarkAutumn@users.noreply.github.com>
2024-01-01 08:31:27 -0800

    Move rewards to their own file

---
+++ DeepQNetwork/zelda/rewards.py
@@ -0,0 +1,91 @@
+from .zelda_game import zelda_game_modes, ZeldaGameState
+
+reward_small = 1.0
+reward_medium = 10.0
+reward_large = 50.0
+
+penalty_small = -1.0
+penalty_medium = -10.0
+penalty_large = -50.0
+
+class ZeldaScoreBasic:
+    def __init__(self):
+        self._locations = set()
+        
+        self.reward_new_location = reward_medium
+        self.reward_enemy_kill = reward_small
+        self.reward_get_rupee = reward_small
+        self.reward_gain_health = reward_medium     # always prioritize health
+
+        self.penalty_lose_beams = penalty_medium
+        self.penalty_take_damage = penalty_small
+        self.penalty_game_over = penalty_large
+
+        self.prev_state = None
+        
+    def __hearts_equal(self, first, second):
+        return abs(first - second) <= 0.01
+    
+    def reset(self):
+        self._locations.clear()
+        self.prev_state = None
+    
+    def score(self, state : ZeldaGameState) -> float:
+        prev = self.prev_state
+        self.prev_state = state
+
+        if prev is None or state is None:
+            return 0
+        
+        reward = 0
+
+        # has the agent found a brand new place?
+        old_location = (prev.level, prev.location)
+        new_location = (state.level, state.location)
+
+        if old_location != new_location:
+            if new_location not in self._locations:
+                self._locations.add(new_location)
+                reward += self.reward_new_location
+                print(f"Reward for discovering new room! {self.reward_new_location}")
+                
+        # did link kill an enemy?
+        if prev.room_kill_count < state.room_kill_count:
+            reward += self.reward_enemy_kill
+            print(f"Reward for killing an enemy!")
+            
+        # did link gain rupees?
+        if prev.rupees_to_add < state.rupees_to_add:
+            # rupees are added to the total one at a time when you pick up multiple, so we
+            # only reward for the accumulator that adds them, not the value of the current
+            # rupee total
+            reward += self.reward_get_rupee
+            print(f"Reward for gaining rupees!")
+
+        if not self.__hearts_equal(prev.hearts, state.hearts):
+            print(f"{prev.hearts} -> {state.hearts}")
+
+        # did link gain health?
+        if prev.hearts < state.hearts:
+            reward += self.reward_gain_health
+            print(f"Reward for gaining hearts!")
+            
+        # did link lose health?
+        elif prev.hearts > state.hearts:
+            # did link lose sword beams as a result?
+            if self.__hearts_equal(prev.hearts, prev.heart_containers):
+                reward += self.penalty_lose_beams
+                print("Penalty for losing beams!");
+            
+            # losing anything other than the first or last heart is less of an issue
+            else:
+                reward += self.penalty_take_damage
+                print("Penalty for losing health!")
+        
+        # did we hit a game over?
+        if prev.game_state != zelda_game_modes.game_over and state.game_state == zelda_game_modes.game_over:
+            reward += self.penalty_game_over
+            print("Penalty for game over!")
+            
+        return reward
+
--- DeepQNetwork/zelda/zelda_game.py
+++ DeepQNetwork/zelda/zelda_game.py
@@ -3,6 +3,7 @@ import ctypes
 import numpy as np
 from collections import deque
 from .dqn import DqnAgentRunner
+from.rewards import ZeldaScoreBasic
 
 # number of frames to give the model
 model_parameter_count = 4
@@ -11,13 +12,8 @@ frames_saved = 60
 
 action_threshold = 0.6 # model must be 60% confident in a button press
 
-reward_small = 1.0
-reward_medium = 10.0
-reward_large = 50.0
-
-penalty_small = -1.0
-penalty_medium = -10.0
-penalty_large = -50.0
+default_scorer = ZeldaScoreBasic
+default_max_memory = 20_000   # number of steps max to keep in memory, at 4 per second, this is 83 minutes of gameplay
 
 class ZeldaGameModes:
     """An enum of game states"""
@@ -177,88 +173,6 @@ class ScreenWrapper:
         return np.frombuffer(self.pointer.contents, dtype=np.uint8).reshape((self.height, self.width, 4))
 
 
-class LegendOfZeldaScorer:
-    def __init__(self):
-        self._locations = set()
-        
-        self.reward_new_location = reward_medium
-        self.reward_enemy_kill = reward_small
-        self.reward_get_rupee = reward_small
-        self.reward_gain_health = reward_medium     # always prioritize health
-
-        self.penalty_lose_beams = penalty_medium
-        self.penalty_take_damage = penalty_small
-        self.penalty_game_over = penalty_large
-
-        self.prev_state = None
-        
-    def __hearts_equal(self, first, second):
-        return abs(first - second) <= 0.01
-    
-    def reset(self):
-        self._locations.clear()
-        self.prev_state = None
-    
-    def score(self, state : ZeldaGameState) -> float:
-        prev = self.prev_state
-        self.prev_state = state
-
-        if prev is None or state is None:
-            return 0
-        
-        reward = 0
-
-        # has the agent found a brand new place?
-        old_location = (prev.level, prev.location)
-        new_location = (state.level, state.location)
-
-        if old_location != new_location:
-            if new_location not in self._locations:
-                self._locations.add(new_location)
-                reward += self.reward_new_location
-                print(f"Reward for discovering new room! {self.reward_new_location}")
-                
-        # did link kill an enemy?
-        if prev.room_kill_count < state.room_kill_count:
-            reward += self.reward_enemy_kill
-            print(f"Reward for killing an enemy!")
-            
-        # did link gain rupees?
-        if prev.rupees_to_add < state.rupees_to_add:
-            # rupees are added to the total one at a time when you pick up multiple, so we
-            # only reward for the accumulator that adds them, not the value of the current
-            # rupee total
-            reward += self.reward_get_rupee
-            print(f"Reward for gaining rupees!")
-
-        if not self.__hearts_equal(prev.hearts, state.hearts):
-            print(f"{prev.hearts} -> {state.hearts}")
-
-        # did link gain health?
-        if prev.hearts < state.hearts:
-            reward += self.reward_gain_health
-            print(f"Reward for gaining hearts!")
-            
-        # did link lose health?
-        elif prev.hearts > state.hearts:
-            # did link lose sword beams as a result?
-            if self.__hearts_equal(prev.hearts, prev.heart_containers):
-                reward += self.penalty_lose_beams
-                print("Penalty for losing beams!");
-            
-            # losing anything other than the first or last heart is less of an issue
-            else:
-                reward += self.penalty_take_damage
-                print("Penalty for losing health!")
-        
-        # did we hit a game over?
-        if prev.game_state != zelda_game_modes.game_over and state.game_state == zelda_game_modes.game_over:
-            reward += self.penalty_game_over
-            print("Penalty for game over!")
-            
-        return reward
-
-
 class ZeldaFrame:
     def __init__(self, frame : int, memory : bytes, screen : bytes, input : bytes):
         self.frame = frame
@@ -294,8 +208,8 @@ class ZeldaFrame:
 
 no_action = [False] * 8
 class LegendOfZeldaAgent:
-    def __init__(self, model, scorer : LegendOfZeldaScorer, max_memory):
-        self.dqn_agent = DqnAgentRunner(LegendOfZeldaScorer().score, model, model.get_random_action, max_memory)
+    def __init__(self, model, scorer = default_scorer, max_memory = default_max_memory):
+        self.dqn_agent = DqnAgentRunner(model, model.get_random_action, max_memory)
         self.prev = None
         self.scorer = scorer
         self.action_threshold = model.action_threshold
