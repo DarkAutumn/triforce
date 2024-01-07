517ff415624407935941c77a18090eb42c9285b4
DarkAutumn <clay.culver@gmail.com>
2024-01-07 04:27:07 -0800

    Fix callback issue

--- retro-stable-project/ppo.py
+++ retro-stable-project/ppo.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import retro
 import os
 import gymnasium as gym
@@ -8,6 +9,8 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.results_plotter import load_results, ts2xy
 
 class Frameskip(gym.Wrapper):
     def __init__(self, env, skip_min, skip_max):
@@ -29,6 +32,54 @@ class Frameskip(gym.Wrapper):
                 break
 
         return obs, total_rew, terminated, truncated, info
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print("Num timesteps: {}".format(self.num_timesteps))
+                print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print("Saving new best model to {}".format(self.save_path))
+                  self.model.save(self.save_path)
+
+        return True
+
+
+
+
 
 def test_environment(env, iterations):
     total_reward = 0
@@ -54,7 +105,8 @@ def train(env, timesteps= 2_000_000):
     model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=log_path)
 
     try:
-        model.learn(timesteps, progress_bar=True)
+        callback = SaveOnBestTrainingRewardCallback(check_freq=10000, log_dir='/models/', verbose=True)
+        model.learn(timesteps, progress_bar=True, callback=callback)
         
         model_path = os.path.join('/models/', f'ppo_combat{timesteps}.zip')
         model.save(model_path)
@@ -74,13 +126,11 @@ def main(action, num):
 
     env = Frameskip(env, 10, 20)
 
-    #eval_callback = EvalCallback(env, best_model_save_path='/models/partial/', eval_freq=100000)
-
     try:
         if action == 'test':
             test_environment(env, num)
 
-        elif action == 'train':
+        elif action == 'train' or action == 'learn':
             train(env, num)
 
         elif action == 'evaluate' or 'record':
