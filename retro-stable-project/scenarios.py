140a764a0c8c672ddc283c1467496585d2f05c9b
DarkAutumn <clay.culver@gmail.com>
2024-01-07 05:59:44 -0800

    Parse arguments, add scenarios

---
+++ retro-stable-project/scenarios.py
@@ -0,0 +1,68 @@
+import os
+import retro
+
+from stable_baselines3 import PPO
+
+from frameskip import Frameskip
+
+class ZeldaScenario:
+    _scenarios = {}
+    _is_initialized = False
+
+    def __init__(self, name, description, algorithm, policy, state, custom_rewards = None):
+        self.name = name
+        self.description = description
+        self.algorithm = algorithm
+        self.policy = policy
+        self.state = state
+        self.custom_rewards = custom_rewards
+
+        ZeldaScenario._scenarios[name] = self
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
+
+    def __repr__(self):
+        return f'{self.name} - {self.description}'
+    
+    @classmethod
+    def initialize(cls):
+        if not cls._is_initialized:
+            script_dir = os.path.dirname(os.path.realpath(__file__))
+            retro.data.Integrations.add_custom_path(os.path.join(script_dir, 'custom_integrations'))
+            cls._is_initialized = True
+
+    @classmethod
+    def get(cls, name):
+        cls.initialize()
+        return cls._scenarios[name]
+    
+    def load_model(self, path):
+        if self.algorithm != 'ppo':
+            raise Exception(f'Unsupported algorithm: {self.algorithm}')
+        
+        return PPO.load(path)
+    
+    def create_model(self, env, **kwargs):
+        if self.algorithm != 'ppo':
+            raise Exception(f'Unsupported algorithm: {self.algorithm}')
+        
+        return PPO('CnnPolicy', env, **kwargs)
+    
+    def create_env(self, **kwargs):
+        env = retro.make(game='Zelda-NES', state=self.state, inttype=retro.data.Integrations.CUSTOM_ONLY, **kwargs)
+
+        # We only take action every so many frames, not every single frame.
+        env = Frameskip(env, 10, 20)
+
+        return env
+    
+    def get_model_name(self, iterations):
+        return f'{self.algorithm}_{self.policy}_{self.name}_{iterations}.zip'
+    
+ZeldaScenario('gauntlet', 'Run link from the starting screen to the furthest right screen without dying.', 'ppo', 'cnn', '120w.state', custom_rewards = None)
+
+def load_scenario(name):
+    return ZeldaScenario.get(name)
+
+__all__ = ['load_scenario']
