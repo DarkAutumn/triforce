3025d519c0b98a29a5b342ae61066dabad3f84cb
DarkAutumn <clay.culver@gmail.com>
2024-01-08 16:40:19 -0800

    Remove the old project

--- old/gym_zelda_nomenu/__init__.py
+++
@@ -1,7 +0,0 @@
-from .zelda_memory import ZeldaMemory
-from .zelda_environment import *
-from .rewards import ZeldaScoreBasic, ZeldaScoreNoHit
-
-import gym
-gym.envs.registration.register(id='Zelda-NoMenu-v0', entry_point='gym_zelda_nomenu:ZeldaNoMenuEnv', nondeterministic=True)
-gym.envs.registration.register(id='Zelda-SmartItems-v0', entry_point='gym_zelda_nomenu:ZeldaSmartItemsEnv', nondeterministic=True)
--- old/zeldaml/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from .zelda_agent import LegendOfZeldaAgent
-from .rewards import *
-from .zelda_constants import zelda_game_modes, zelda_memory_layout
-from .zelda_frame import ZeldaFrame
-from .zelda_actions import get_movement, get_action, link_actions, link_movements
-from .zelda_state import ZeldaGameState
