6a8ba172bb0922a2a265c4d9e9a7c41907e34221
DarkAutumn <clay.culver@gmail.com>
2024-01-12 13:21:07 -0800

    Use room kills instead of kill streak to calculate total kills

--- triforce_lib/damage_detector.py
+++ triforce_lib/damage_detector.py
@@ -27,7 +27,7 @@ class DamageDetector(gym.Wrapper):
         self.consume_injury = False
 
         self.total_kills = 0
-        self.last_kill_streak = 0
+        self.last_room_kills = 0
         self.total_injuries = 0
 
         self.damage_table = [0] * 12
@@ -44,14 +44,15 @@ class DamageDetector(gym.Wrapper):
             self.beams_handled = False
             self.consume_kill = False
             self.consume_injury = False
+            self.last_room_kills = info['room_kills']
             self.fill_damage_table(info, self.damage_table)
             self.update_info(info)
             return obs, rewards, terminated, truncated, info
 
         beam_state = get_beam_state(info)
 
-        if self.last_kill_streak < info['kill_streak']:
-            self.total_kills += info['kill_streak'] - self.last_kill_streak
+        if self.last_room_kills < info['room_kills']:
+            self.total_kills += info['room_kills'] - self.last_room_kills
             
             if beam_state == 2 and self.consume_kill:
                 self.total_kills -= 1
@@ -86,14 +87,14 @@ class DamageDetector(gym.Wrapper):
                 
             self.total_injuries += injuries
 
-        # don't let anyone use kill_streak, they should use total_kills
-        self.last_kill_streak = info['kill_streak']
+        # don't let anyone use room_kills, they should use total_kills
+        self.last_room_kills = info['room_kills']
         self.update_info(info)
         
         return obs, rewards, terminated, truncated, info
 
     def update_info(self, state):
-        del state['kill_streak']
+        del state['room_kills']
         state['total_kills'] = self.total_kills
         state['total_injuries'] = self.total_injuries
 
@@ -114,7 +115,7 @@ class DamageDetector(gym.Wrapper):
     def did_beams_kill(self, act, state):
         self.beams_handled = True
         savestate = self.env.unwrapped.em.get_state()
-        original_kills = state['kill_streak']
+        original_kills = state['room_kills']
 
         damage_table = [0] * 12
         self.fill_damage_table(state, damage_table)
@@ -123,14 +124,14 @@ class DamageDetector(gym.Wrapper):
         beams = get_beam_state(state)
         while beams == 1 and not is_mode_death(state['mode']):
             frames += 1
-            original_kills = state['kill_streak'] # update in case we get damaged and streak resets
+            original_kills = state['room_kills'] # update in case we get damaged and streak resets
             
             self.fill_damage_table(state, damage_table)
             obs, rewards, terminated, truncated, state = self.env.step(act)
             beams = get_beam_state(state)
 
         self.env.unwrapped.em.set_state(savestate)
-        kills = state['kill_streak'] > original_kills
+        kills = state['room_kills'] > original_kills
 
         # check damage
         prev_damage = damage_table.copy()
