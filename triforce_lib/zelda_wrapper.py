c7e0516e4dc12bca14a8aae24a4d1cdbffd22533
DarkAutumn <clay.culver@gmail.com>
2024-01-13 07:55:14 -0800

    Create ZeldaGameWrapper
    - Instead of having a frameskip and a wrapper for info, unify into one wrapper that handles producing new game info and holding down the button for actions.
    - Implemented better beam and kill detection.

--- triforce_lib/zelda_wrapper.py
+++ triforce_lib/zelda_wrapper.py
@@ -4,140 +4,183 @@
 #
 # This consumes some state and produces 'total_kills' and 'total_injuries'.
 
+from typing import Any
 import gymnasium as gym
-from .zelda_game import is_mode_death, get_beam_state
 
-class ZeldaWrapper(gym.Wrapper):
+from .zelda_game_data import zelda_game_data
+from .model_parameters import actions_per_second
+from .zelda_game import is_mode_death, get_beam_state, is_mode_scrolling
+
+class ZeldaObjectData:
+    def __init__(self, slot, ram = None):
+
+        for table, (offset, size) in zelda_game_data.tables.items():
+            self.__dict__[table.name] = ram[offset:offset+size]
+
+    @property
+    def link_pos(self):
+        return self.get_position(0)
+    
+    def get_position(self, obj : int):
+        return self.obj_pos_x[obj], self.obj_pos_y[obj]
+    
+    def get_object_id(self, obj : int):
+        if obj == 0:
+            return None
+
+        return self.obj_id[obj]
+    
+    def get_obj_direction(self, obj : int):
+        return self.obj_direction[obj]
+    
+    def get_obj_health(self, obj : int):
+        if obj == 0:
+            return None
+        return self.obj_health[obj] >> 4
+        
+
+    def is_enemy(self, obj_id : int):
+        return 1 <= obj_id <= 0x48
+    
+    def enumerate_enemy_ids(self) -> int:
+        enemies = 0
+        for i in range(1, 0xb):
+            if self.is_enemy(self.get_object_id(i)):
+                yield i
+
+# Frame skip values based on actions per second
+frameskip_ranges = {
+    1: (58, 62),      # one action every ~60 frames
+    2: (30, 50),      # one action every ~40 frames
+    3: (20, 30),      # one action every ~20 frames
+    4: (10, 20),      # one action every ~15 frames
+    5: (9, 15),       # one action every ~12 frames
+}
+
+class ZeldaGameWrapper(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
 
-        self._clear()
+        frameskip_min, frameskip_max = frameskip_ranges[actions_per_second]
+        self._skip_min = frameskip_min
+        self._skip_max = frameskip_max
+
+        self._reset_state()
 
     def reset(self, **kwargs):
-        self._clear()
-        obs, info = self.env.reset(**kwargs)
-
-        info['total_kills'] = self.total_kills
-        info['total_injuries'] = self.total_injuries
-
-        return obs, info
+        result = super().reset(**kwargs)
+        self._reset_state()
+        return result
     
-    def _clear(self):
-        self.beams_handled = False
-        self.consume_kill = False
-        self.consume_injury = False
-
-        self.total_kills = 0
-        self.last_room_kills = 0
-        self.total_injuries = 0
-
-        self.damage_table = [0] * 12
-        self.prev_location = (-1, -1)
-        
-
+    def _reset_state(self):
+        self._location = None
+        self._beams_already_active = False
+        self._prev_enemies = None
+        self._prev_health = None
+    
     def step(self, act):
+        # take the first step
         obs, rewards, terminated, truncated, info = self.env.step(act)
 
-        # do nothing if this is a new screen
-        location = (info['level'], info['location'])
-        if location != self.prev_location:
-            self.prev_location = location
-            self.beams_handled = False
-            self.consume_kill = False
-            self.consume_injury = False
-            self.last_room_kills = info['room_kills']
-            self.fill_damage_table(info, self.damage_table)
-            self.update_info(info)
-            return obs, rewards, terminated, truncated, info
-
-        beam_state = get_beam_state(info)
-
-        if self.last_room_kills < info['room_kills']:
-            self.total_kills += info['room_kills'] - self.last_room_kills
+        # frameskip
+        for i in range(self._skip_min, self._skip_max):           
+            obs, rew, terminated, truncated, info = self.env.step(act)
+            rewards += rew
+            if terminated or truncated:
+                return obs, rewards, terminated, truncated, info
             
-            if beam_state == 2 and self.consume_kill:
-                self.total_kills -= 1
-                self.consume_kill = False
+        # skip scrolling
+        while is_mode_scrolling(info["mode"]):
+            obs, rew, terminated, truncated, info = self.env.step(act)
+            rewards += rew
+            if terminated or truncated:
+                break
 
-        if beam_state == 0:
-            self.consume_kill = False
-            self.beams_handled = False
-            self.consume_injury = False
-
-        elif beam_state == 1:
-            if not self.beams_handled:
-                kill, injury = self.did_beams_kill(act, info)
-                self.consume_kill = kill
-                self.consume_injury = injury
-                if kill:
-                    self.total_kills += 1
-                if injury:
-                    self.total_injuries += 1
-
-        else:
-            self.beams_handled = False
-
-        # injuries are  hits that are not fatal
-        prev_damage = self.damage_table.copy()
-        self.fill_damage_table(info, self.damage_table)
-        injuries = self.detect_injuries(prev_damage, self.damage_table)
-        if injuries:
-            if self.consume_injury:
-                injuries -= 1
-                self.consume_injury = False
-                
-            self.total_injuries += injuries
-
-        # don't let anyone use room_kills, they should use total_kills
-        self.last_room_kills = info['room_kills']
-        self.update_info(info)
+        unwrapped = self.env.unwrapped
+        objects = ZeldaObjectData(unwrapped.get_ram())
+        beam_hits = 0
+        step_kills = 0
+        step_injuries = 0
         
+        location = (info['level'], info['location'])
+        new_location = self._location != location
+
+        # capture enemy health data
+        curr_enemy_health = {}
+        for eid in objects.enumerate_enemy_ids():
+            assert self._prev_health is None or eid in self._prev_health
+            curr_enemy_health[eid] = objects.get_obj_health(eid)
+
+        # only check beams and other state if we are in the same room:
+        if new_location:
+            self._location = location
+            self._prev_health = None
+        else:
+            # check if beams are still active after frameskips
+            beams = get_beam_state(info)
+            if beams == 1:
+                # Process if beams weren't active when we left this function
+                if not self._beams_already_active:
+                    kills, injuries = self.did_hit_during(info, act, info, lambda st: get_beam_state(st) == 1)
+                    beam_hits = kills + injuries
+                    self._beams_already_active = True
+            else:
+                self._beams_already_active = False
+
+            # check if we killed or injured anything
+            if self._prev_health:
+                # check kills
+                for eid, health in self._prev_health:
+                    if eid not in curr_enemy_health:
+                        step_kills += 1
+
+                    elif curr_enemy_health[eid] < health:
+                        step_injuries += 1
+
+        info['objects'] = objects
+        self._prev_health = curr_enemy_health
+        self._prev_objs = objects
+
+        info['beams_hit'] = beam_hits
+        info['step_kills'] = step_kills
+        info['step_injuries'] = step_injuries
+        info['new_location'] = new_location
+
         return obs, rewards, terminated, truncated, info
 
-    def update_info(self, state):
-        del state['room_kills']
-        state['total_kills'] = self.total_kills
-        state['total_injuries'] = self.total_injuries
+    def did_hit_during(self, act, info, should_continue):
+        unwrapped = self.env.unwrapped
+        savestate = unwrapped.em.get_state()
+        data = unwrapped.data
 
-    def detect_injuries(self, prev_health, curr_health):
+        objects = info['objects']
+        start_enemies = list(objects.enumerate_enemy_ids())
+        start_health = {x: objects.get_obj_health(x) for x in start_enemies}
+        
+        # Step over until should_continue is false, or until we left this room or hit a termination condition.
+        # Update info at each iteration.
+        location = (info['level'], info['location'])
+
+        while should_continue(info) and not is_mode_death(info['mode']) and location == (info['level'], info['location']):
+            data.set_value('hearts_and_containers', 0xff) # make sure we don't die
+
+            _, _, terminated, truncated, info = self.env.step(act)
+            if terminated or truncated:
+                break
+        
+        # compare enemies to previous
+        kills = 0
         injuries = 0
-        for x in range(12):
-            prev = prev_health[x] >> 4
-            curr = curr_health[x] >> 4
-            if curr > 0 and curr < prev:
+
+        end_enemies = list(objects.enumerate_enemy_ids())
+        if len(end_enemies) < len(start_enemies):
+            kills += len(start_enemies) - len(end_enemies)
+
+        for enemy in end_enemies:
+            start = start_health.get(enemy, 0)
+            end = objects.get_obj_health(enemy)
+            if end < start:
                 injuries += 1
 
-        return injuries
-    
-    def fill_damage_table(self, state, table):
-        for x in range(12):
-            table[x] = state[f"enemy_health_{x}"]
-
-    def did_beams_kill(self, act, state):
-        self.beams_handled = True
-        savestate = self.env.unwrapped.em.get_state()
-        original_kills = state['room_kills']
-
-        damage_table = [0] * 12
-        self.fill_damage_table(state, damage_table)
-
-        frames = 0
-        beams = get_beam_state(state)
-        while beams == 1 and not is_mode_death(state['mode']):
-            frames += 1
-            original_kills = state['room_kills'] # update in case we get damaged and streak resets
-            
-            self.fill_damage_table(state, damage_table)
-            obs, rewards, terminated, truncated, state = self.env.step(act)
-            beams = get_beam_state(state)
-
-        self.env.unwrapped.em.set_state(savestate)
-        kills = state['room_kills'] > original_kills
-
-        # check damage
-        prev_damage = damage_table.copy()
-        self.fill_damage_table(state, damage_table)
-        injuries = self.detect_injuries(prev_damage, damage_table) > 0
-
+        unwrapped.em.set_state(savestate)
         return kills, injuries
-            
