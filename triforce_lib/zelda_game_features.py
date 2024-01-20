f411f6da1098441bc6ae608d41571a70968ffd94
DarkAutumn <clay.culver@gmail.com>
2024-01-19 16:18:09 -0800

    Push vectors up to game wrapper

--- triforce_lib/zelda_game_features.py
+++ triforce_lib/zelda_game_features.py
@@ -29,43 +29,19 @@ class ZeldaGameFeatures(gym.Wrapper):
         vectors = self.get_enemy_vectors(info)
         return {"image": observation, "enemy_vectors": vectors}
 
-    def normalize_vector(self, vector):
-        """Normalize a 2D vector."""
-        norm = np.linalg.norm(vector)
-        if norm == 0: 
-            return vector
-        return vector / norm
-    
     def get_enemy_vectors(self, info):
         if info is None or 'link_pos' not in info or 'objects' not in info:
             return np.zeros((self.num_enemy_vectors, 2), dtype=np.float32)
-
-        link_pos = np.array(info['link_pos'], dtype=np.float32)
-        objects = info['objects']
-
-        closest_enemy = self.get_closest_normalized(link_pos, objects, objects.enumerate_enemy_ids())
-        closest_projectile = self.get_closest_normalized(link_pos, objects, objects.enumerate_projectile_ids())
-        closest_item = self.get_closest_normalized(link_pos, objects, objects.enumerate_item_ids())
+        closest_enemy = self.get_vector_of_closest(info['enemy_vectors'])
+        closest_projectile = self.get_vector_of_closest(info['projectile_vectors'])
+        closest_item = self.get_vector_of_closest(info['item_vectors'])
 
         # create an np array of the vectors
         normalized_vectors = [np.zeros(2, dtype=np.float32), closest_enemy, closest_projectile, closest_item, np.zeros(2, dtype=np.float32)]
         return np.array(normalized_vectors, dtype=np.float32)
 
-    def get_closest_normalized(self, link_pos, objects, ids):
-        positions = [objects.get_position(id) for id in ids if id is not None]
-
-        # Calculate vectors and distances to each enemy
-        vectors_and_distances = [
-            ((enemy_pos - link_pos), np.linalg.norm(enemy_pos - link_pos))
-            for enemy_pos in positions
-        ]
-
-        epsilon = 1e-6  # Prevent division by zero
-
-        # Sort by distance
-        vectors_and_distances.sort(key=lambda x: x[1])
-        normalized_vectors = [self.normalize_vector(v[0]) for v in vectors_and_distances if abs(v[1]) > epsilon]
-        if normalized_vectors:
-            return normalized_vectors[0]
+    def get_vector_of_closest(self, vectors_and_distances):
+        if vectors_and_distances:
+            return vectors_and_distances[0][0]
         
         return np.zeros(2, dtype=np.float32)
