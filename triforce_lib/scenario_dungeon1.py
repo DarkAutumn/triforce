05528006513c62abcdbd334bd6406acab44cc3ff
DarkAutumn <clay.culver@gmail.com>
2024-01-23 10:08:26 -0800

    Add Dungeon 1 scenario

---
+++ triforce_lib/scenario_dungeon1.py
@@ -0,0 +1,44 @@
+import typing
+
+from .critic import ZeldaGameplayCritic
+from .end_condition import ZeldaEndCondition
+from .zelda_game import get_num_triforce_pieces
+
+class Dungeon1Critic(ZeldaGameplayCritic):
+    def __init__(self):
+        super().__init__()
+
+        self.health_change_reward = self.reward_large
+
+class Dungeon1EndCondition(ZeldaEndCondition):
+    def clear(self):
+        super().clear()
+        self._new_rooms = set()
+        self._frame_count = 0
+
+    def is_scenario_ended(self, old_state : typing.Dict[str, int], new_state : typing.Dict[str, int]):
+        terminated, truncated, reason = super().is_scenario_ended(old_state, new_state)
+
+        self._frame_count += 1
+
+        if not terminated and not truncated:
+            if new_state['level'] != 1:
+                reason = "left-scenario"
+                terminated = True
+
+            location = new_state['location']
+            if location not in self._new_rooms:
+                self._new_rooms.add(location)
+                self._frame_count = 0
+            else:
+                self._frame_count += 1
+                if self._frame_count > 800:
+                    reason = "no-discovery-timeout"
+                    terminated = True
+
+            if get_num_triforce_pieces(old_state) < get_num_triforce_pieces(new_state):
+                reason = "gained-triforce"
+                terminated = True
+
+        return terminated, truncated, reason
+    
