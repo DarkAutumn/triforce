4ddcdbf858a97eb74c5ffb322ca4c139a3a21e27
DarkAutumn <clay.culver@gmail.com>
2024-01-27 12:01:16 -0800

    Increase parallelism

--- train_with_tensorboard.sh
+++ train_with_tensorboard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python triforce.py train --parallel 10 --output /training &
+python triforce.py train --parallel 16 --output /training &
 TRAIN_PID=$!
 tensorboard --logdir=/training --bind_all &
 TENSORBOARDPID=$!
