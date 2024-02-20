e584870ed9a88da2df4919032d559704c0541110
DarkAutumn <clay.culver@gmail.com>
2024-02-20 08:35:10 -0800

    Fix init warnings

--- triforce_lib/__init__.py
+++ triforce_lib/__init__.py
@@ -1,4 +1,9 @@
+"""
+The triforce library is a collection of tools for training and running AI models for the NES game The Legend of Zelda.
+"""
+
 import os
+import json
 import retro
 
 from .zelda_orchestrator import ZeldaAIOrchestrator
@@ -12,13 +17,6 @@ from .zelda_game import is_in_cave
 script_dir = os.path.dirname(os.path.realpath(__file__))
 retro.data.Integrations.add_custom_path(os.path.join(script_dir, 'custom_integrations'))
 
-# parse json
-import json
-with open(os.path.join(script_dir, 'triforce.json')) as f:
-    all_settings = json.load(f)
-    ZeldaScenario.initialize(all_settings['scenarios'])
-    ZeldaModel.initialize(all_settings['models'])
-
 # define the model surface area
 __all__ = [
     ZeldaML.__name__,
@@ -28,3 +26,15 @@ __all__ = [
     ZeldaAIOrchestrator.__name__,
     is_in_cave.__name__,
     ]
+
+def __init__():
+    """
+    Initialize the triforce library.
+
+    This module 
+    """
+
+    with open(os.path.join(script_dir, 'triforce.json'), encoding='utf-8') as f:
+        all_settings = json.load(f)
+        ZeldaScenario.initialize(all_settings['scenarios'])
+        ZeldaModel.initialize(all_settings['models'])
