069f4359c7287e146313f573ecb18b04c5f15bcc
DarkAutumn <DarkAutumn@users.noreply.github.com>
2024-02-23 10:39:55 -0800

    Add a small cooldown when entering a room to allow tiles to full load (#50)

--- triforce/model_parameters.py
+++ triforce/model_parameters.py
@@ -6,6 +6,7 @@ VIEWPORT_PIXELS = 128
 # the y coordinate where the game viewport starts (above which is the HUD)
 GAMEPLAY_START_Y = 56
 
+LOCATION_CHANGE_COOLDOWN = 8
 MOVEMENT_FRAMES = 6
 ATTACK_COOLDOWN = 15
 ITEM_COOLDOWN = 10
