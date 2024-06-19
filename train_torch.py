e34d9dc73e82ee351eb90bba147b564cd479f099
DarkAutumn <DarkAutumn@users.noreply.github.com>
2024-06-18 19:29:08 -0700

    Render mode

--- train_torch.py
+++ train_torch.py
@@ -14,6 +14,7 @@ def parse_args():
     parser.add_argument("--iterations", type=int, default=-1, help="Override iteration count.")
     parser.add_argument("--log-dir", type=str, help="Location to write logs to.")
     parser.add_argument("--intermediate", type=str, help="Location to save intermediate models to.")
+    parser.add_argument("--render", action='store_true', help="Render the game.")
 
     try:
         args = parser.parse_args()
@@ -48,7 +49,8 @@ def main(args):
     iterations = 10_000 if args.iterations <= 0 else args.iterations
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    env = make_multihead_zelda_env('0_67s.state', device=device)
+    render_mode = 'human' if args.render else None
+    env = make_multihead_zelda_env('0_67s.state', device=device, render_mode=render_mode)
     try:
         obs_space = env.observation_space
         image_dim = obs_space[0].shape[-1]
