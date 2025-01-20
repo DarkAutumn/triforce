b349cb752a3d5c7fc4dc7e6fb68275403e0f95a2
DarkAutumn <DarkAutumn@users.noreply.github.com>
2025-01-20 06:21:16 -0800

    Add NextRoomTimeout

--- triforce/end_conditions.py
+++ triforce/end_conditions.py
@@ -68,6 +68,48 @@ class Timeout(ZeldaEndCondition):
 
         return False, False, None
 
+
+class NextRoomTimeout(ZeldaEndCondition):
+    """End the scenario if the agent is in the same screen position, or fails to discover a new room."""
+    def __init__(self):
+        super().__init__()
+        self.__position_duration = 0
+        self.__last_discovery = 0
+
+        # the number of timesteps the agent can be in the same position before we truncate
+        self.position_timeout = 50
+        self.no_discovery_timeout = 1200
+
+    def clear(self):
+        self.__position_duration = 0
+        self.__last_discovery = 0
+
+    def is_scenario_ended(self, state_change : StateChange) -> tuple[bool, bool, str]:
+        # Check if link is stuck in one position on the screen
+        prev, curr = state_change.previous, state_change.state
+
+        if self.position_timeout:
+            if prev.link.position == curr.link.position and not state_change.hits:
+                if self.__position_duration >= self.position_timeout:
+                    return False, True, "failure-stuck"
+
+                self.__position_duration += 1
+            else:
+                self.__position_duration = 0
+
+        # Check if link never found a new room
+        if self.no_discovery_timeout:
+            if prev.location != curr.location and curr.full_location in prev.objectives.next_rooms:
+                self.__last_discovery = 0
+
+            else:
+                self.__last_discovery += 1
+
+            if self.__last_discovery > self.no_discovery_timeout:
+                return False, True, "failure-no-next-room"
+
+        return False, False, None
+
 class StartingSwordCondition(ZeldaEndCondition):
     """End conditions for 'pick up the sword' scenario."""
     def is_scenario_ended(self, state_change : StateChange) -> tuple[bool, bool, str]:
