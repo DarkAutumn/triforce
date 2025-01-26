ca86a3eedfcc15f80010f686adbe7af6aaa1d17d
DarkAutumn <DarkAutumn@users.noreply.github.com>
2025-01-26 06:54:13 -0800

    Trailing whitespace

--- triforce/end_conditions.py
+++ triforce/end_conditions.py
@@ -143,7 +143,7 @@ class LeftDungeon(ZeldaEndCondition):
     def is_scenario_ended(self, state_change : StateChange) -> tuple[bool, bool, str]:
         if state_change.state.level == 0:
             return True, False, "failure-left-dungeon"
-        
+
         if any(x.id == ZeldaEnemyKind.WallMaster for x in state_change.previous.enemies) \
                 and state_change.previous.full_location.manhattan_distance(state_change.state.full_location) > 1:
             return True, False, "failure-wallmastered"
